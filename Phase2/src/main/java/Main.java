59d5fb6dc4029ff59b5c4d15f9dd780e27de4f75
mjmaher987 <mjmaher987@gmail.com>
2021-07-26 13:04:53 +0430

    final commit of Phase2

---
+++ Phase2/src/main/java/Main.java
@@ -0,0 +1,24 @@
+package main.java;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        preprocesses();
+    }
+
+    public static void preprocesses() {
+        File directoryPath = new File("C:\\Users\\mjmah\\IdeaProjects\\codestar-intern-issues\\src\\main\\resources\\EnglishData");
+        File[] filesList = directoryPath.listFiles();
+        try {
+            InvertedIndex idx = new InvertedIndex();
+            assert filesList != null;
+            for (File file : filesList) {
+                idx.indexFile(file);
+            }
+            new TakeInput(idx);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Phase2/src/main/java/TakeInput.java
@@ -0,0 +1,64 @@
+package main.java;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TakeInput {
+
+    public TakeInput(InvertedIndex idx) {
+        getOrder(idx);
+    }
+
+    public void getOrder(InvertedIndex idx) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            String[] inputSplit = input.split("(\\s+)");
+            if (input.equals("--back")) break;
+            ArrayList<String> plusStrings = new ArrayList<>();
+            ArrayList<String> minusStrings = new ArrayList<>();
+            ArrayList<String> normalStrings = new ArrayList<>();
+            for (String string : inputSplit)
+                addItemToOneOfThreeArrayLists(string, plusStrings, minusStrings, normalStrings);
+            showResult(processes(idx, plusStrings, minusStrings, normalStrings));
+        }
+    }
+
+    private void showResult(Set<String> answer) {
+        for (String s : answer) System.out.println(s);
+
+    }
+
+    private Set<String> processes(InvertedIndex idx, ArrayList<String> plusStrings,
+                                  ArrayList<String> minusStrings, ArrayList<String> normalStrings) {
+        Set<String> answer = idx.search(plusStrings);
+        Set<String> toDelete = idx.search(minusStrings);
+        ArrayList<Set<String>> commons = new ArrayList<>();
+        for (String normalString : normalStrings) {
+            ArrayList<String> arrayList = new ArrayList<>();
+            arrayList.add(normalString);
+            commons.add(idx.search(arrayList));
+        }
+        answer = idx.findCommonFiles(answer, commons);
+        answer = idx.deleteGivenFiles(answer, toDelete);
+        return answer;
+    }
+
+    private void addItemToOneOfThreeArrayLists(String string, ArrayList<String> plusStrings,
+                                               ArrayList<String> minusStrings, ArrayList<String> normalStrings) {
+        Pattern pattern = Pattern.compile("^\\+(.+)$");
+        Matcher matcher = pattern.matcher(string);
+        Pattern pattern1 = Pattern.compile("^-(.+)$");
+        Matcher matcher1 = pattern1.matcher(string);
+        if (matcher.find()) {
+            String a = matcher.group(1);
+            plusStrings.add(a);
+        } else if (matcher1.find()) {
+            String a = matcher1.group(1);
+            minusStrings.add(a);
+        } else normalStrings.add(string);
+    }
+
+
+}
