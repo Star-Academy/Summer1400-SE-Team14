ebd539fbf580f492eb748102a15f9af189b56335
mjmaher987 <mjmaher987@gmail.com>
2021-07-27 13:09:44 +0430

    Code Review Problem reported by Mr. Mostmand number 9 repaired

--- Phase2/src/main/java/Main.java
+++ Phase2/src/main/java/Main.java
@@ -1,31 +1,10 @@
 package main.java;
 
-import java.io.File;
-import java.net.URL;
+
 
 public class Main {
     public static void main(String[] args) {
-        preprocesses();
+        PreProcessing.preprocesses();
     }
 
-    public static void preprocesses() {
-        URL url = Main.class.getResource("../resources/EnglishData");
-        if (url == null) System.out.println("INVALID PATH!");
-        else {
-            File directoryPath = new File(url.getPath());
-            File[] filesList = directoryPath.listFiles();
-            try {
-                InvertedIndex idx = new InvertedIndex();
-                if (filesList == null) System.out.println("INVALID FILE LIST!");
-                else {
-                    for (File file : filesList) {
-                        idx.indexFile(file);
-                    }
-                    new TakeInput(idx);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
---
+++ Phase2/src/main/java/PreProcessing.java
@@ -0,0 +1,27 @@
+package main.java;
+
+import java.io.File;
+import java.net.URL;
+
+public class PreProcessing {
+    public static void preprocesses() {
+        URL url = Main.class.getResource("../resources/EnglishData");
+        if (url == null) System.out.println("INVALID PATH!");
+        else {
+            File directoryPath = new File(url.getPath());
+            File[] filesList = directoryPath.listFiles();
+            try {
+                InvertedIndex idx = new InvertedIndex();
+                if (filesList == null) System.out.println("INVALID FILE LIST!");
+                else {
+                    for (File file : filesList) {
+                        idx.indexFile(file);
+                    }
+                    new TakeInput(idx);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
