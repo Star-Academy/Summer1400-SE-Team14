f5db7e043e54fe2cf467aef4afb020289f953f5f
mjmaher987 <mjmaher987@gmail.com>
2021-07-27 16:55:36 +0430

    tests for first 9 methods of class InvertedIndex were added

--- src/test/java/JavaTest.java
+++ src/test/java/JavaTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class JavaTest {
     @Test
@@ -17,5 +19,18 @@ public class JavaTest {
         List<String> filesTest = new ArrayList<>();
         filesTest.add(file.getPath());
         Assertions.assertEquals(filesTest, invertedIndex.getFiles());
+    }
+
+    @Test
+    public void searchTest() throws IOException {
+        File file = new File(PreProcessing.class.getResource("/EnglishData/57110").getPath());
+        InvertedIndex invertedIndex = new InvertedIndex();
+        invertedIndex.indexFile(file);
+
+        ArrayList<String> wordsToSearch = new ArrayList<>();
+        wordsToSearch.add("friend");
+        Set<String> pathRoots = new HashSet<>();
+        pathRoots.add(file.getPath());
+        Assertions.assertEquals(invertedIndex.search(wordsToSearch), pathRoots);
     }
 }
