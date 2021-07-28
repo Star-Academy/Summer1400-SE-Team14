a0c72fee8f0cdf8e39053a9ec1686ea25e6e9f44
mjmaher987 <mjmaher987@gmail.com>
2021-07-28 17:50:56 +0430

    3 tests were added - reached to 85% Code Coverage

--- src/test/java/JavaTest.java
+++ src/test/java/JavaTest.java
@@ -63,6 +63,28 @@ public class JavaTest {
         Assertions.assertEquals("INVALID PATH!", PreProcessing.preprocesses("/EnglishDAta/12", ""));
     }
 
+
+    @Test
+    public void getOrderTest() throws IOException {
+        File file = new File(PreProcessing.class.getResource("/EnglishData/57110").getPath());
+        InvertedIndex invertedIndex = new InvertedIndex();
+        invertedIndex.indexFile(file);
+        InputScanner inputScanner = new InputScanner(invertedIndex);
+        Set<String> set = new HashSet<>();
+        set.add(file.getPath());
+        Assertions.assertEquals(set, inputScanner.getOrder("friend"));
+    }
+
+    @Test
+    public void fetOrderPlusStrings() throws IOException {
+        File file = new File(PreProcessing.class.getResource("/EnglishData/57110").getPath());
+        InvertedIndex invertedIndex = new InvertedIndex();
+        invertedIndex.indexFile(file);
+        InputScanner inputScanner = new InputScanner(invertedIndex);
+        Set<String> set = new HashSet<>();
+        set.add(file.getPath());
+        Assertions.assertEquals(set, inputScanner.getOrder("+friend"));
+    }
 //    @Test
 //    public void giveInput() throws IOException {
 //        File file = new File(PreProcessing.class.getResource("/EnglishData/57110").getPath());
