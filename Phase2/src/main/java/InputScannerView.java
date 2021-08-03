57e95e0b894a37a4f7857b4510b95c9a8f9bb2e2
parsasharify <sharifyparsa1381@gmail.com>
2021-08-02 23:22:06 -0700

    changed code part2

--- Phase2/src/main/java/InputScannerView.java
+++ Phase2/src/main/java/InputScannerView.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class InputScannerView {
+    final String backString = "--back";
+
     public InputScannerView(InvertedIndex index) {
         Scanner scanner = new Scanner(System.in);
         InputScanner inputScanner = new InputScanner(index);
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("--back")) break;
+            if (input.equals(backString)) break;
             showResult(inputScanner.getOrder(input));
         }
         scanner.close();
