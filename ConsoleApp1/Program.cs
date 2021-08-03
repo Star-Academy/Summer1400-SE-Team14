973cd99af1c7e03456e8763a8f6db9dde508d0f9
parsasharify <sharifyparsa1381@gmail.com>
2021-08-03 05:29:16 -0700

    first test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,11 +2,16 @@
 
 namespace ConsoleApp1
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public int ASD(int a, int b)
+        {
+            return a + b;
         }
     }
 }
