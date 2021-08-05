0546269a983fec61680daa609ff1d93b876a6300
parsasharify <sharifyparsa1381@gmail.com>
2021-08-05 08:49:37 -0700

    added new interface

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -7,14 +7,14 @@ namespace TestProject1
 {
     public class UnitTest1
     {
-        [Fact]
+           [Fact]
         public void TestDeleteGivenFiles()
         {
             IInvertedIndex invertedIndex = new InvertedIndex();
-            HashSet<string> set = new  HashSet<string>();
+            HashSet<string> set = new HashSet<string>();
             set.Add("asd");
             set.Add("qwe");
-            HashSet<string> set2 = new  HashSet<string>();
+            HashSet<string> set2 = new HashSet<string>();
             set2.Add("asd");
             List<string> set3 = new List<string>();
             set3.Add("qwe");
@@ -62,9 +62,41 @@ namespace TestProject1
 
 
         [Fact]
-        public void TakingInput()
+        public void test()
         {
-            
+            IInvertedIndex invertedIndex = new InvertedIndex();
+            invertedIndex.IndexFile("C:\\Users\\ASUS\\RiderProjects\\Phase-5\\EnglishData\\");
+            List<string> filesTest = new List<string>();
+            filesTest.Add(@"C:\Users\ASUS\RiderProjects\Phase-5\EnglishData\57110");
+            Assert.Equal(filesTest, invertedIndex.GetFiles());
         }
+
+        [Fact]
+        public void searchTest()
+        {
+            InvertedIndex invertedIndex = new InvertedIndex();
+            invertedIndex.IndexFile("C:\\Users\\ASUS\\RiderProjects\\Phase-5\\EnglishData\\");
+
+            List<string> wordsToSearch = new List<string>();
+            wordsToSearch.Add("friend");
+            HashSet<string> pathRoots = new HashSet<string>();
+            pathRoots.Add(@"C:\Users\ASUS\RiderProjects\Phase-5\EnglishData\57110");
+            Assert.Equal(invertedIndex.Search(wordsToSearch), pathRoots);
+        }
+
+        [Fact]
+        public void checkDeleteGivenFiles() {
+            InvertedIndex invertedIndex = new InvertedIndex();
+            HashSet<string> set = new HashSet<string>();
+            set.Add("asd");
+            set.Add("qwe");
+            HashSet<string> set2 = new HashSet<string>();
+            set2.Add("asd");
+            HashSet<string> set3 = new HashSet<string>();
+            set3.Add("qwe");
+            Assert.Equal(set3, invertedIndex.DeleteGivenFiles(set, set2));
+        }
+        
+        
     }
 }
