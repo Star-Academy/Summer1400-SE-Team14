b8f316aa48c031ebadd5eb4095073ec32ca64f32
parsasharify <sharifyparsa1381@gmail.com>
2021-08-09 05:33:07 -0700

    added filepath

---
+++ ConsoleApp1/FilePath.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class FilePath
+    {
+        public static List<FilePath> AllFilePaths;
+        public string filePath { set; get; }
+    }
+}
