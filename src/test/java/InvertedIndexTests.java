6c3cdeb06b29d6234d019a3f90eca0b9322bd527
mjmaher987 <mjmaher987@gmail.com>
2021-08-10 16:27:03 +0430

    Problem number 10 reported by Mr. Kasaei is repaired

--- src/test/java/InvertedIndexTests.java
+++ src/test/java/InvertedIndexTests.java
@@ -25,7 +25,6 @@ public class InvertedIndexTests {
         File file = new File(PreProcessing.class.getResource("/EnglishData/57110").getPath());
         InvertedIndex invertedIndex = new InvertedIndex();
         invertedIndex.indexFile(file);
-
         ArrayList<String> wordsToSearch = new ArrayList<>();
         wordsToSearch.add("friend");
         Set<String> pathRoots = new HashSet<>();
@@ -34,19 +33,33 @@ public class InvertedIndexTests {
     }
 
     @Test
-    public void checkFindCommonWords() {
+    public void checkFindCommonWordsForEmptySituation() {
+        InvertedIndex invertedIndex = new InvertedIndex();
+        ArrayList<Set<String>> arrayList = new ArrayList<>();
+        Assertions.assertEquals(invertedIndex.findCommonWords(arrayList), new HashSet<>());
+    }
+
+    @Test
+    public void checkFirstFindCommonWordsForUnEmptySituation() {
         InvertedIndex invertedIndex = new InvertedIndex();
         Set<String> set = new HashSet<>();
         ArrayList<Set<String>> arrayList = new ArrayList<>();
-        Assertions.assertEquals(invertedIndex.findCommonWords(arrayList), new HashSet<>());
         arrayList.add(set);
         set.add("asd");
         Assertions.assertEquals(set, invertedIndex.findCommonWords(arrayList));
+    }
+
+    @Test
+    public void checkSecondFindCommonWordsForUnEmptySituation() {
+        InvertedIndex invertedIndex = new InvertedIndex();
+        Set<String> set = new HashSet<>();
+        ArrayList<Set<String>> arrayList = new ArrayList<>();
+        arrayList.add(set);
+        set.add("asd");
         Set<String> set2 = new HashSet<>();
         set2.add("ass");
         arrayList.add(set2);
         Assertions.assertEquals(set, invertedIndex.findCommonWords(arrayList));
-
     }
 
     @Test
@@ -62,7 +75,15 @@ public class InvertedIndexTests {
     }
 
     @Test
-    public void checkFindCommonFiles(){
+    public void checkFindCommonFiles1(){
+        InvertedIndex invertedIndex = new InvertedIndex();
+        Set<String> answer = new HashSet<>();
+        ArrayList<Set<String>> arrayList = new ArrayList<>();
+        Assertions.assertEquals(new HashSet<>(), invertedIndex.findCommonFiles(answer , arrayList));
+    }
+
+    @Test
+    public void checkFindCommonFiles2(){
         InvertedIndex invertedIndex = new InvertedIndex();
         Set<String> answer = new HashSet<>();
         Set<String> set = new HashSet<>();
@@ -71,14 +92,33 @@ public class InvertedIndexTests {
         Set<String> set2 = new HashSet<>();
         set2.add("asd");
         ArrayList<Set<String>> arrayList = new ArrayList<>();
-        Assertions.assertEquals(new HashSet<>(), invertedIndex.findCommonFiles(answer , arrayList));
-
         arrayList.add(set);
         arrayList.add(set2);
         Assertions.assertEquals(set2, invertedIndex.findCommonFiles(answer , arrayList));
-        answer.add("asd");
 
+    }
+
+    @Test
+    public void checkFindCommonFiles3(){
+        InvertedIndex invertedIndex = new InvertedIndex();
+        Set<String> answer = new HashSet<>();
+        Set<String> set = new HashSet<>();
+        set.add("asd");
+        set.add("qwe");
+        Set<String> set2 = new HashSet<>();
+        set2.add("asd");
+        ArrayList<Set<String>> arrayList = new ArrayList<>();
+        arrayList.add(set);
+        arrayList.add(set2);
+        answer.add("asd");
         Assertions.assertEquals(answer, invertedIndex.findCommonFiles(answer , arrayList));
+    }
+
+    @Test
+    public void checkFindCommonFiles4(){
+        InvertedIndex invertedIndex = new InvertedIndex();
+        Set<String> answer = new HashSet<>();
+        answer.add("asd");
         Assertions.assertEquals(answer, invertedIndex.findCommonFiles(answer , new  ArrayList<Set<String>>()));
     }
 
