54db0f44bf284a5ea291b1cb455991f4e8067170
parsasharify <sharifyparsa1381@gmail.com>
2021-08-10 23:50:01 -0700

    adding interface

---
+++ ConsoleApp1/INormalize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public interface INormalize
+    {
+        List<string> NormalizeInputWords(List<string> wordsToFind);
+
+        string ConvertToLowerCase(string wordsInFiles);
+        void ConvertFileToTokens(int fileNumber, string filePath);
+    }
+}
