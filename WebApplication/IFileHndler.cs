0a06089863f10421449995a73745712bd688eea1
parsasharify <sharifyparsa1381@gmail.com>
2021-08-16 07:55:26 -0700

    adding classes

---
+++ WebApplication/IFileHndler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public interface IFileHandler
+    {
+        void AddFileNumbers(List<FileInfo> fileInfoList, HashSet<string> answer);
+
+
+
+
+        void ImportWordsInList(string line, int fileNumber);
+
+        List<string> GetFiles();
+    }
+}
